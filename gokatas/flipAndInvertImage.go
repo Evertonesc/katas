4261cba2c8e86d474e9bb8919477dcf5b8243a2d
esc <evertonsouzaesc@gmail.com>
2024-05-23 20:57:50 -0300

    flip and invert image problem solution

---
+++ gokatas/flipAndInvertImage.go
@@ -0,0 +1,28 @@
+package gokatas
+
+func flipAndInvertImage(image [][]int) [][]int {
+	for i := 0; i < len(image); i++ {
+		last := len(image[i]) - 1
+		for j := 0; j < len(image[i]); j++ {
+
+			if j != last {
+				image[i][j], image[i][last] = image[i][last], image[i][j]
+				last--
+			}
+
+			if j == last {
+				for k := 0; k < len(image[i]); k++ {
+					if image[i][k] == 0 {
+						image[i][k] = 1
+					} else {
+						image[i][k] = 0
+					}
+				}
+
+				break
+			}
+		}
+	}
+
+	return image
+}
